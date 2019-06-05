d2f332e74f7e204161bdac287366c29e89d6f38d
natalie-kramer <natalie-kramer@users.noreply.github.com>
2019-06-05 14:04:20 -0600

    Add files via upload

--- UpscaleWrapper.R
+++ UpscaleWrapper.R
@@ -2,16 +2,20 @@
 #Natalie Kramer (n.kramer.anderson@gmail.com)
 #Last Updated JUne 4, 2019
 
+#documentation is provided here:
+# https://github.com/natalie-kramer/GeomorphicUpscale/edit/master/docs/upscaling.md
+
 ###################################
 ####User Defined Inputs############
 ##################################
 
-PROJdir="E:\\AsotinUpscale"
-GUPdir="E:\\GitHub\\GeomorphicUpscale"
 
-braid.index="E://AsotinUpscale//Inputs//braid_index.csv"
-selections="E://AsotinUpscale//Inputs//selections.csv" 
-network="E://AsotinUpscale//Inputs//network.csv"
+PROJdir="" #path to your project directory
+GUPdir=" " #path to the GeomorphicUpscale repository"
+
+braid.index=" " ##path to braid.index.csv
+selections=" " ##path to selections.csv" 
+network=" " ##path to network.csv"
 
 #Builds the project directory structure and re-organizes inputs
 source(paste(GUPdir, "\\scripts\\projbuild.R", sep=""))
@@ -24,50 +28,30 @@ head(network)
 #####################################
 ####User Defined Variables############
 #####################################
-
-####Description of User	Variables#####		
-#
-#layer: Specifies which GUT output 
-#	      #Options: "Tier3_InChannel", "Tier2_InChannel_Transition" 
-#model: Specifies which response model
-#	      #Options "NREI", "HSI"  #*Currently only available for Steelhead
-#ROI: Specifies you region of interest as bankfull (bf), hydro modelling extent (hydro), or extent of fish bearing modelling values (hab)
-#	      #Options "bf", "hydro", "hab"
-#responsevar: Specifies your response type as total count of fish (No.Fish), # Fish/ROI area in m2 (Density), % of BF area with fish bearing modelling values (Hab),Median Modelling Value witin bf extent (MedModelVal)						
-#	      #Options "No.Fish", "Density", "Hab", "MedModelVal"
-#poolby: Specifies how you want to average responses: over the entire dataset (none), pooling by river styles (RS) or pooling by each river style and condition varient (RSCond)
-#	      #Options: "RS", "RScond", "none" 
-#plottype: Specifies format of file extension for plots. If none, they won't be saved to a folder.
-#	      #Optinos: #".tiff", ".png", ".pdf", "none"
-#segmentID: column header name for reach segment ID in network input file
-#distcol: column header name for length of segment in m in network input file
-#bfwcol: column header name for estimates of bankful width in network input file
-#condcols: vector of column headers for condition scenarios in network input file
-#		   #e.g. c("Condition0","Condition1", "Condition2")
-#RScol: column header name for RiverStyle code in network input file. 
-#	    #Should match river style codes provided in selections file.
-########################################
+#These are set to some defaults.  Please see documentation for options.
+#https://natalie-kramer.github.io/GeomorphicUpscale/upscaling.html
 
 layer="Tier3_InChannel"
 makeplot=T				
 plottype=".tiff"
 
-model="NREI" 				
-ROI="bf"   					
-responsevar="Density" 			 
-poolby="RScond"	
+#response.R script is not updated to new data format yet
+#model="NREI" 	
+#species="steelhead"
+#lifestage="adult"
+#ROI="bf"   					
+#responsevar="Density" 			 
+#poolby="RScond"	
 	
-upscalevar="Capacity"
-method="bfDensity"
-segIDcol="segmentID"
-distcol="Length_m"
-bfwcol="BFWest"
-condcols=c("Condition0","Condition1", "Condition2")
-RScol="RS"			
-validatenetwork=F
-
-###########################################
-##########################################
+#response.R script is not updated to new data format yet
+#upscalevar="Capacity"
+#method="bfDensity"
+#segIDcol="segmentID"
+#lengthcol="length.m"
+#widthcol="bf.width.m"
+#condcols=c("Condition0","Condition1", "Condition2")
+#RScol="RS"			
+#validatenetwork=F
 
 ############################################
 ######Geenerate Estimates and run the Upscale
@@ -76,11 +60,14 @@ validatenetwork=F
 ######Generating Assemblage Estimates##
 source(paste(GUPdir, "\\scripts\\assemblage.R", sep=""))
 
+
 ######Generating Response Files########
-source(paste(GUPdir, "\\scripts\\response.R", sep=""))
+#This SCRIPT DOES NOT YET MATCH NEW DATA FORMAT
+#source(paste(GUPdir, "\\scripts\\response.R", sep=""))
 
 ######Generating Upscale Files########
-source(paste(GUPdir, "\\scripts\\upscale.R", sep=""))
+#This SCRIPT DOES NOT YET MATCH NEW DATA FORMAT
+#source(paste(GUPdir, "\\scripts\\upscale.R", sep=""))
 
 
 ########################################
